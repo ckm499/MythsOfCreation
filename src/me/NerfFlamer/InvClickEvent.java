6bb77f490d5c32931483bb749db5038f670cc9fe
NerfFlamer <pokemonly@live.com>
2015-07-23 20:24:36 -0700

    Update InvClickEvent.java

--- src/me/NerfFlamer/InvClickEvent.java
+++ src/me/NerfFlamer/InvClickEvent.java
@@ -1,9 +1,11 @@
 package me.NerfFlamer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InvClickEvent implements Listener {
 	
@@ -22,14 +24,18 @@ public class InvClickEvent implements Listener {
 		}
 		
 		if(e.getInventory().getName().contains("Shop")) {
+			e.setCancelled(true);
 			Integer pointamount = plugin.pointData.get(player.getUniqueId());
 			
-			Integer price = Integer.parseInt(e.getCurrentItem().getItemMeta().getLore().get(2));
+			Integer price = Integer.parseInt(e.getCurrentItem().getItemMeta().getLore().get(1));
 			if(pointamount <= price) {
-				
-				
-				
+				player.sendMessage(ChatColor.RED + "You do not have enough coins!");
 			}
+			plugin.pointData.put(player.getUniqueId(), pointamount-price);
+			player.sendMessage(ChatColor.GREEN + "You have purchased %item% for %price%!"
+					.replaceAll("%item%", e.getCurrentItem().getType().name())
+					.replaceAll("%price%", e.getCurrentItem().getItemMeta().getLore().get(1)));
+			player.getInventory().addItem(new ItemStack(e.getCurrentItem().getType()));
 		}
 	}
 	
