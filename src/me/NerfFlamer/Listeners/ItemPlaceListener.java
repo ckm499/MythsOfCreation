09f92dd6b3bfd5f13761d637cf8da42a9b33d65a
ckm499 <calebkmitchell@gmail.com>
2015-08-12 23:38:56 -0400

    Fixed Error on Interact With Normal Items

--- src/me/NerfFlamer/Listeners/ItemPlaceListener.java
+++ src/me/NerfFlamer/Listeners/ItemPlaceListener.java
@@ -13,65 +13,61 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class ItemPlaceListener implements Listener{
-	
+public class ItemPlaceListener implements Listener {
+
 	@EventHandler
-	public void onPlace(PlayerInteractEvent e)
-	{
+	public void onPlace(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
 		PerkEffects effect = new PerkEffects();
 		List<String> perks = YamlConfiguration.loadConfiguration(
 				new File("plugins" + File.separator + "MythsOfCreation"
 						+ File.separator + "PlayerData" + File.separator
 						+ p.getUniqueId() + ".yml")).getStringList("perks");
-		if ((p.getItemInHand().getItemMeta().getLore().get(0).equals("Fury") || 
-				p.getItemInHand().getItemMeta().getLore().get(0).equals("Sheild") || 
-				p.getItemInHand().getItemMeta().getLore().get(0).equals("Sprint") || 
-				p.getItemInHand().getItemMeta().getLore().get(0).equals("Regen")) && 
-				(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR))
-		{
-			if (e.getPlayer().getItemInHand().getItemMeta().getLore().get(0).equals("Fury"))
-			{
-				if (effect.fury(perks, p))
-				{
-					return;
+		if (p.getItemInHand().getItemMeta().hasLore()) {
+			if ((p.getItemInHand().getItemMeta().getLore().get(0)
+					.equals("Fury")
+					|| p.getItemInHand().getItemMeta().getLore().get(0)
+							.equals("Sheild")
+					|| p.getItemInHand().getItemMeta().getLore().get(0)
+							.equals("Sprint") || p.getItemInHand()
+					.getItemMeta().getLore().get(0).equals("Regen"))
+					&& (e.getAction() == Action.RIGHT_CLICK_BLOCK || e
+							.getAction() == Action.RIGHT_CLICK_AIR)) {
+				if (e.getPlayer().getItemInHand().getItemMeta().getLore()
+						.get(0).equals("Fury")) {
+					if (effect.fury(perks, p)) {
+						return;
+					} else {
+						p.sendMessage(ChatColor.DARK_RED
+								+ "You must purchase this perk before using it!");
+					}
 				}
-				else {
-					p.sendMessage(ChatColor.DARK_RED
-					    + "You must purchase this perk before using it!");
+				if (e.getPlayer().getItemInHand().getItemMeta().getLore()
+						.get(0).equals("Sheild")) {
+					if (effect.sheild(perks, p)) {
+						return;
+					} else {
+						p.sendMessage(ChatColor.DARK_RED
+								+ "You must purchase this perk before using it!");
+					}
 				}
-			}
-			if (e.getPlayer().getItemInHand().getItemMeta().getLore().get(0).equals("Sheild"))
-			{
-				if (effect.sheild(perks, p))
-				{
-					return;
+				if (e.getPlayer().getItemInHand().getItemMeta().getLore()
+						.get(0).equals("Sprint")) {
+					if (effect.sprint(perks, p)) {
+						return;
+					} else {
+						p.sendMessage(ChatColor.DARK_RED
+								+ "You must purchase this perk before using it!");
+					}
 				}
-				else {
-					p.sendMessage(ChatColor.DARK_RED
-					    + "You must purchase this perk before using it!");
-				}
-			}
-			if (e.getPlayer().getItemInHand().getItemMeta().getLore().get(0).equals("Sprint"))
-			{
-				if (effect.sprint(perks, p))
-				{
-					return;
-				}
-				else {
-					p.sendMessage(ChatColor.DARK_RED
-					    + "You must purchase this perk before using it!");
-				}
-			}
-			if (e.getPlayer().getItemInHand().getItemMeta().getLore().get(0).equals("Regen"))
-			{
-				if (effect.regen(perks, p))
-				{
-					return;
-				}
-				else {
-					p.sendMessage(ChatColor.DARK_RED
-					    + "You must purchase this perk before using it!");
+				if (e.getPlayer().getItemInHand().getItemMeta().getLore()
+						.get(0).equals("Regen")) {
+					if (effect.regen(perks, p)) {
+						return;
+					} else {
+						p.sendMessage(ChatColor.DARK_RED
+								+ "You must purchase this perk before using it!");
+					}
 				}
 			}
 		}
