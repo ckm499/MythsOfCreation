bda928be7e5a4ad042ee07221fbb01e214f96e34
ckm499 <calebkmitchell@gmail.com>
2015-08-18 15:45:16 -0400

    Fixed Error When Item Didnt Have Lore

--- src/me/NyxCraft/Listeners/ItemDropListener.java
+++ src/me/NyxCraft/Listeners/ItemDropListener.java
@@ -6,19 +6,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
-public class ItemDropListener implements Listener{
-	
+public class ItemDropListener implements Listener {
+
 	@EventHandler
-	public void onPlayerDropItem(PlayerDropItemEvent e)
-	{
+	public void onPlayerDropItem(PlayerDropItemEvent e) {
 		Player p = e.getPlayer();
-		if (e.getItemDrop().getItemStack().getItemMeta().getLore().get(0).equals("Fury") ||
-				e.getItemDrop().getItemStack().getItemMeta().getLore().get(0).equals("Sheild") ||
-				e.getItemDrop().getItemStack().getItemMeta().getLore().get(0).equals("Sprint") ||
-				e.getItemDrop().getItemStack().getItemMeta().getLore().get(0).equals("Regen"))
-		{
-			e.setCancelled(true);
-			p.sendMessage(ChatColor.DARK_RED + "You can't drop this item!");
+		if (e.getItemDrop().getItemStack().getItemMeta().hasLore()) {
+			if (e.getItemDrop().getItemStack().getItemMeta().getLore().get(0)
+					.equals("Fury")
+					|| e.getItemDrop().getItemStack().getItemMeta().getLore()
+							.get(0).equals("Sheild")
+					|| e.getItemDrop().getItemStack().getItemMeta().getLore()
+							.get(0).equals("Sprint")
+					|| e.getItemDrop().getItemStack().getItemMeta().getLore()
+							.get(0).equals("Regen")) {
+				e.setCancelled(true);
+				p.sendMessage(ChatColor.DARK_RED + "You can't drop this item!");
+			}
 		}
 	}
 }
